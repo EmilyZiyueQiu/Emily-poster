a37bb4e2d9742677f1e00108a02225bcf7ad2c9b
EmilyZiyueQiu <61444207+EmilyZiyueQiu@users.noreply.github.com>
2020-03-03 16:27:56 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,27 @@ function setup() {
 }
 
 function draw() {	
+	//tool for mouse position
+	console.log(mouseX,mouseY);
+
+	//no stroke
+	noStroke();
+
+	//poster background (white)
+	fill(250,245,232);
+	rect(420, 0,600,windowHeight);
+
+
+	//color yellow
+	fill(235,198,79);
+	rect(450,30,540,650);
+
+	//color white(face)
+	fill(250,245,232);
+	rect(630,30,180,650);
 	
+
+
 
 }
 
